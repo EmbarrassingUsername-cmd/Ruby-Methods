b6c2248c3656298357188fc1afbae55553c2d461
Jamie Bukraba <Jamie.Bukraba@googlemail.com>
2021-02-13 23:35:55 +0000

    removed needless local variable in substrings method

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,9 +1,8 @@
 def substrings(string, dictionary)
-  array_to_test = string.downcase.split
-  #splits string by spaces to compare each word
-  array_to_test.reduce(Hash.new(0)) do |result_hash, word_to_test|
+  string.downcase.split.reduce(Hash.new(0)) do |result_hash, word_to_test|
     dictionary.each { |word_to_include| result_hash[word_to_include] += 1 if word_to_test.include?(word_to_include)}
     result_hash
+  #creates an array of words from the downcased string then using that   
   #creates Hash with default value of zero as the accumulator checks for each word in dictonary in each word in string and increments hash value 
   #at the key of each dictionary word by 1 if included 
   end
